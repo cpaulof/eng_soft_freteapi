15edc7dbf4b6516da418446132995a792d9f5ae9
marcellodinizr <marcellodinizrocha@gmail.com>
2022-12-06 10:37:04 -0300

    adding lombok in models

--- src/main/java/edu/ifma/lpweb/freteapi/model/Cidade.java
+++ src/main/java/edu/ifma/lpweb/freteapi/model/Cidade.java
@@ -5,80 +5,28 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 public class Cidade {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @NotEmpty(message="Campo nome e obrigatorio")
-    private String nome;
+	@NotEmpty(message = "Campo nome e obrigatorio")
+	private String nome;
 
-    @NotEmpty(message="Campo UF e obrigatorio")
-    private String uf;
+	@NotEmpty(message = "Campo UF e obrigatorio")
+	private String uf;
 
-    @NotNull(message="Campo taxa e obrigatorio")
-    @Min(value=1, message="Campo taxa nao e valido")
-    private BigDecimal taxa;
+	@NotNull(message = "Campo taxa e obrigatorio")
+	@Min(value = 1, message = "Campo taxa nao e valido")
+	private BigDecimal taxa;
 
-    public Cidade() {
-    }
-
-
-    public Cidade(String nome, String uf, BigDecimal taxa) {
-        this.nome = nome;
-        this.uf = uf;
-        this.taxa = taxa;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getUf() {
-        return this.uf;
-    }
-
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public BigDecimal getTaxa() {
-        return this.taxa;
-    }
-
-    public void setTaxa(BigDecimal taxa) {
-        this.taxa = taxa;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cidade cidade = (Cidade) o;
-        return Objects.equals(id, cidade.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
--- src/main/java/edu/ifma/lpweb/freteapi/model/Frete.java
+++ src/main/java/edu/ifma/lpweb/freteapi/model/Frete.java
@@ -1,102 +1,50 @@
 package edu.ifma.lpweb.freteapi.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+
 import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
 public class Frete {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    private String descricao;
+	private String descricao;
 
-    @NotNull(message="Campo peso e obrigatorio")
-    private Float peso;
+	@NotNull(message = "Campo peso e obrigatorio")
+	private Float peso;
 
-    @NotNull(message="Campo valor e obrigatorio")
-    private BigDecimal valor;
+	@NotNull(message = "Campo valor e obrigatorio")
+	private BigDecimal valor;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    private Cliente cliente;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Cliente cliente;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    private Cidade cidade;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Cidade cidade;
 
-    public Frete() {
-    }
+	public Frete(String descricao, Float peso, BigDecimal valor, Cliente cliente, Cidade cidade) {
+		this.descricao = descricao;
+		this.peso = peso;
+		this.valor = valor;
+		this.cliente = cliente;
+		this.cidade = cidade;
+	}
 
-    public Frete(String descricao, Float peso, BigDecimal valor, Cliente cliente, Cidade cidade) {
-        this.descricao = descricao;
-        this.peso = peso;
-        this.valor = valor;
-        this.cliente = cliente;
-        this.cidade = cidade;
-    }
+	public BigDecimal calcularFrete() {
+		// R$10,00 é o valor fixo para o cálculo
+		this.valor = new BigDecimal(this.peso * 10).add(this.cidade.getTaxa());
+		return this.getValor();
 
-    public Frete(int id, String descricao, Float peso, BigDecimal valor, Cliente cliente, Cidade cidade) {
-        this(descricao, peso, valor, cliente, cidade);
-        this.id = id;
-    }
-
-
-    public int getId() {
-        return this.id;
-    }
-
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Cliente getCliente() {
-        return this.cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-
-    public Cidade getCidade() {
-        return this.cidade;
-    }
-
-    public void setCidade(Cidade cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getDescricao() {
-        return this.descricao;
-    }
-
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Float getPeso() {
-        return this.peso;
-    }
-
-
-    public void setPeso(Float peso) {
-        this.peso = peso;
-    }
-
-    public BigDecimal getValor() {
-        return this.valor;
-    }
-
-    public BigDecimal calcularFrete() {
-        // R$10,00 é o valor fixo para o cálculo
-        this.valor = new BigDecimal(this.peso * 10).add(this.cidade.getTaxa() );
-        return this.getValor();
-
-    }
+	}
 
 }
